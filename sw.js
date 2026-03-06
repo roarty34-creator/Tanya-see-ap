aaa733c6368c30bf2bdfab15c9fd0c4186205cdf
roarty34-creator <roarty34@gmail.com>
2026-03-06 11:44:40 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,37 @@
-const CACHE="tanya-v36"
+const CACHE_NAME = "tanya-deepsea-v32";
 
-self.addEventListener("install",e=>{
-self.skipWaiting()
-})
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./?v=32"
+];
 
-self.addEventListener("fetch",e=>{
-e.respondWith(fetch(e.request))
-})
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
