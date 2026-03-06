a7e27572d3843a0d0fa997c3da8a7402791f1e26
roarty34-creator <roarty34@gmail.com>
2026-03-06 21:05:25 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "tanya-v37";
+const CACHE_NAME = "tanya-v38";
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
   "./icon-192.png",
   "./icon-512.png",
-  "./?v=37"
+  "./?v=38"
 ];
 
 self.addEventListener("install", (event) => {
@@ -20,9 +20,7 @@ self.addEventListener("activate", (event) => {
     caches.keys().then((keys) =>
       Promise.all(
         keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
+          if (key !== CACHE_NAME) return caches.delete(key);
         })
       )
     ).then(() => self.clients.claim())
