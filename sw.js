56881ce9b42af2f3b728a87473c36c25620ef801
roarty34-creator <roarty34@gmail.com>
2026-03-06 08:48:59 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,32 @@
-const CACHE = "tanya-sea-v29";
+const CACHE = "tanya-deepsea-v35";
 const ASSETS = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
   "./icon-192.png",
-  "./icon-512.png"
+  "./icon-512.png",
+  "./?v=35"
 ];
 
-// Install: cache core files
 self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(CACHE).then((c) => c.addAll(ASSETS))
-  );
   self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
-// Activate: remove old caches
 self.addEventListener("activate", (e) => {
   e.waitUntil(
     caches.keys().then((keys) =>
-      Promise.all(keys.map((k) => (k !== CACHE ? caches.delete(k) : null)))
-    )
+      Promise.all(
+        keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))
+      )
+    ).then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
-// Fetch: cache-first for app shell
 self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then((r) => r || fetch(e.request))
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
