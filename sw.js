d40efb2ec00f3a9db2470be3aefc5cbd06cf8b34
roarty34-creator <roarty34@gmail.com>
2026-03-06 10:40:17 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
-const CACHE_NAME="tanya-v33";
+const CACHE_NAME = "tanya-v35";
 
-self.addEventListener("install",e=>{
+self.addEventListener("install", e => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate",e=>{
+self.addEventListener("activate", e => {
   e.waitUntil(self.clients.claim());
 });
 
-self.addEventListener("fetch",e=>{
+self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request))
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
