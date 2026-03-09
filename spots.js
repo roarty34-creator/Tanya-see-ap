31a1f00192878ddac1d195002160a2aeaa111d48
roarty34-creator <roarty34@gmail.com>
2026-03-09 11:59:23 +0200

    Update spots.js

--- spots.js
+++ spots.js
@@ -1,23 +1,31 @@
-const fishingSpots = [
-  {
-    name: "Skulpbank",
-    lat: -34.4120,
-    lng: 20.8350,
-    depth: "30-50m",
-    fish: "Kabeljou, Reef Fish"
-  },
-  {
-    name: "Blinde Rif",
-    lat: -34.4015,
-    lng: 20.8801,
-    depth: "40-60m",
-    fish: "Kabeljou"
-  },
-  {
-    name: "Sand se Bank",
-    lat: -34.3980,
-    lng: 20.8520,
-    depth: "35-45m",
-    fish: "Reef Fish"
-  }
-];
+const SPOTS = [
+
+{
+name:"Skulpbank",
+lat:-34.3905,
+lon:20.8602,
+type:"Reef"
+},
+
+{
+name:"Blinde Rif",
+lat:-34.4015,
+lon:20.8801,
+type:"Reef"
+},
+
+{
+name:"Kabeljou Ridge",
+lat:-34.3750,
+lon:20.8700,
+type:"Reef"
+},
+
+{
+name:"Roman Bank",
+lat:-34.4200,
+lon:20.8650,
+type:"Reef"
+}
+
+]
