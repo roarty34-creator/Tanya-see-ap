4894dbe4b7e55389368fb425fd757e8788a1e2a2
roarty34-creator <roarty34@gmail.com>
2026-03-09 10:14:36 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -19,9 +19,7 @@ self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
       )
     )
   );
