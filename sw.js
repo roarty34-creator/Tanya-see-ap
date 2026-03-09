85c3ee8f67191e99c4eb9af1f46a1d2dc69c305a
roarty34-creator <roarty34@gmail.com>
2026-03-09 15:02:03 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,53 +1,47 @@
-const CACHE_NAME="tanya-deep-sea-v78"
+const CACHE_NAME = "tanya-deep-sea-v79";
 
-const APP_SHELL=[
-"./",
-"./index.html",
-"./manifest.webmanifest",
-"./icon-192.png",
-"./icon-512.png"
-]
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
 
-self.addEventListener("install",event=>{
-event.waitUntil(
-caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL))
-)
-self.skipWaiting()
-})
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
 
-self.addEventListener("activate",event=>{
-event.waitUntil(
-caches.keys().then(keys=>
-Promise.all(
-keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))
-)
-)
-)
-self.clients.claim()
-})
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
 
-self.addEventListener("fetch",event=>{
+self.addEventListener("fetch", event => {
+  if(event.request.method !== "GET") return;
 
-if(event.request.method!=="GET") return
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if(cached) return cached;
 
-event.respondWith(
-caches.match(event.request).then(cached=>{
+      return fetch(event.request).then(response => {
+        const copy = response.clone();
 
-if(cached) return cached
+        if(event.request.url.startsWith(self.location.origin)){
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        }
 
-return fetch(event.request).then(response=>{
-
-const copy=response.clone()
-
-if(event.request.url.startsWith(self.location.origin)){
-caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy))
-}
-
-return response
-
-}).catch(()=>caches.match("./index.html"))
-
-})
-)
-
-})
+        return response;
+      }).catch(() => caches.match("./index.html"));
+    })
+  );
+});
