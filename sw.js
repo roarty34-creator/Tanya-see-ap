d128d098667d7072696b754e89249c4f14a19bad
roarty34-creator <roarty34@gmail.com>
2026-03-09 12:22:44 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,44 @@
-const CACHE_NAME="tanya-deep-sea-v75"
+const CACHE_NAME = "tanya-deep-sea-v75";
+const FILES = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./spots.js",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
 
-const FILES=[
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+  self.skipWaiting();
+});
 
-"./",
-"./index.html",
-"./style.css",
-"./app.js",
-"./spots.js",
-"./manifest.webmanifest"
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
 
-]
+self.addEventListener("fetch", event => {
+  if(event.request.method !== "GET") return;
 
-self.addEventListener("install",e=>{
-
-e.waitUntil(
-
-caches.open(CACHE_NAME)
-
-.then(c=>c.addAll(FILES))
-
-)
-
-})
-
-self.addEventListener("fetch",e=>{
-
-e.respondWith(
-
-caches.match(e.request)
-
-.then(r=>r||fetch(e.request))
-
-)
-
-})
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if(cached) return cached;
+      return fetch(event.request).then(response => {
+        const copy = response.clone();
+        if(event.request.url.startsWith(self.location.origin)){
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        }
+        return response;
+      }).catch(() => caches.match("./index.html"));
+    })
+  );
+});
