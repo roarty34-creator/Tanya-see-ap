588bced651640bbc330184ce01f12e0307ba8e29
roarty34-creator <roarty34@gmail.com>
2026-03-10 18:54:57 +0200

    Update sw.js
    V105

--- sw.js
+++ sw.js
@@ -1,19 +1,29 @@
-const CACHE_NAME="tanya-v105";
-
-const FILES=[
-"./",
-"./index.html",
-"./manifest.webmanifest"
+const CACHE_NAME = "tanya-deep-sea-v105";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest"
 ];
 
-self.addEventListener("install",e=>{
-e.waitUntil(
-caches.open(CACHE_NAME).then(c=>c.addAll(FILES))
-)
-})
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
 
-self.addEventListener("fetch",e=>{
-e.respondWith(
-caches.match(e.request).then(r=>r||fetch(e.request))
-)
-})
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if(event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
