d4615915bde46e04da237afb24ca2b6c4503f3a1
roarty34-creator <roarty34@gmail.com>
2026-03-12 21:13:01 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,41 @@
-const CACHE_NAME = "tanya-deep-sea-v107-fixed-2";
+const CACHE_NAME = "tanya-deep-sea-v108-clean";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
   "./icon-192.png",
-  "./icon-512.png"
+  "./icon-512.png",
+
+  "./fish-cape-cob.png",
+  "./fish-yellowtail.png",
+  "./fish-bonito.png",
+  "./fish-red-roman.png",
+  "./fish-snapper.png",
+  "./fish-silverfish.png",
+  "./fish-red-steenbras.png",
+  "./fish-yellow-belly.png",
+  "./fish-hottentot.png",
+  "./fish-geelbek.png",
+  "./fish-carpenter.png",
+  "./fish-dageraad.png",
+  "./fish-santer.png",
+  "./fish-elf-shad.png",
+  "./fish-miss-lucy.png",
+
+  "./rig-cape-cob.png",
+  "./rig-yellowtail.png",
+  "./rig-red-roman.png",
+  "./rig-snapper.png",
+  "./rig-silverfish.png",
+  "./rig-red-steenbras.png",
+  "./rig-yellow-belly.png",
+  "./rig-hottentot.png",
+  "./rig-carpenter.png",
+  "./rig-dageraad.png",
+  "./rig-santer.png",
+  "./rig-elf-shad.png",
+  "./rig-miss-lucy.png"
 ];
 
 self.addEventListener("install", event => {
@@ -32,8 +62,6 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request);
-    })
+    caches.match(event.request).then(cached => cached || fetch(event.request))
   );
 });
